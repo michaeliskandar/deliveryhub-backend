8290553644c6219cdb5927663d94cb941fc32b34
Mohameed547 <mohamedzohair547@gmail.com>
2026-06-18 22:53:59 +0300

    merge done

--- src/shared/middleware/authenticate.js
+++ src/shared/middleware/authenticate.js
@@ -30,37 +30,37 @@ export const authenticate = async (req, res, next) => {
         }
     }
 };
-import jwt from "jsonwebtoken";
-import ApiError from "../utils/ApiError.js";
-import { ENV } from "../../config/env.js";
-import User from "../../database/models/User.model.js";
+// import jwt from "jsonwebtoken";
+// import ApiError from "../utils/ApiError.js";
+// import { ENV } from "../../config/env.js";
+// import User from "../../database/models/User.model.js";
 
-export const authenticate = async (req, res, next) => {
-    try {
-        const header = req.headers.authorization;
+// export const authenticate = async (req, res, next) => {
+//     try {
+//         const header = req.headers.authorization;
 
-        if (!header || !header.startsWith("Bearer ")) {
-            throw new ApiError(401, "No token provided");
-        }
+//         if (!header || !header.startsWith("Bearer ")) {
+//             throw new ApiError(401, "No token provided");
+//         }
 
-        const token = header.split(" ")[1];
+//         const token = header.split(" ")[1];
 
-        let decoded;
-        try {
-            decoded = jwt.verify(token, ENV.JWT_SECRET);
-        } catch {
-            throw new ApiError(401, "Invalid or expired token");
-        }
+//         let decoded;
+//         try {
+//             decoded = jwt.verify(token, ENV.JWT_SECRET);
+//         } catch {
+//             throw new ApiError(401, "Invalid or expired token");
+//         }
 
-        // decoded is expected to carry { id } at minimum (set at login time)
-        const user = await User.findById(decoded.id).select("-passwordHash");
-        if (!user) {
-            throw new ApiError(401, "User no longer exists");
-        }
+//         // decoded is expected to carry { id } at minimum (set at login time)
+//         const user = await User.findById(decoded.id).select("-passwordHash");
+//         if (!user) {
+//             throw new ApiError(401, "User no longer exists");
+//         }
 
-        req.user = user;
-        return next();
-    } catch (err) {
-        return next(err);
-    }
-};
+//         req.user = user;
+//         return next();
+//     } catch (err) {
+//         return next(err);
+//     }
+// };
